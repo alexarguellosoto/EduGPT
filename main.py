a712220f2887332ff5bfaf03b1146bb5aa59a42c
alexarguellosoto <alexarguellosoto@gmail.com>
2026-08-15 04:56:16 -0600

    Implementar pipeline de preparacion de EduGPT

--- main.py
+++ main.py
@@ -1,44 +1,96 @@
 from src.models.model import EduGPT
+
 from src.datasets.corpus_builder import CorpusBuilder
+
 from src.tokenizer.tokenizer import Tokenizer
+
+from src.training.trainer import Trainer
 
 
 def main():
+
     model = EduGPT()
+
     builder = CorpusBuilder()
+
     tokenizer = Tokenizer()
 
+    trainer = Trainer(builder)
+
     while True:
+
         option = model.show_menu()
 
+        # ---------------------------------
+
         if option == "1":
+
             builder.build_corpus()
 
+        # ---------------------------------
+
         elif option == "2":
-            model.train()
+
+            model.train(trainer)
+
+        # ---------------------------------
 
         elif option == "3":
+
             model.show_information()
 
+        # ---------------------------------
+
         elif option == "4":
+
             corpus = builder.load_corpus()
-            tokenizer.run(corpus, "manual")
+
+            tokenizer.run(
+                corpus,
+                "manual"
+            )
+
+        # ---------------------------------
 
         elif option == "5":
+
             corpus = builder.load_corpus()
-            tokenizer.run(corpus, "nltk")
+
+            tokenizer.run(
+                corpus,
+                "nltk"
+            )
+
+        # ---------------------------------
 
         elif option == "6":
+
             corpus = builder.load_corpus()
-            tokenizer.run(corpus, "tiktoken")
+
+            tokenizer.run(
+                corpus,
+                "tiktoken"
+            )
+
+        # ---------------------------------
 
         elif option == "0":
-            print("Saliendo del programa...")
+
+            print(
+                "Saliendo del programa..."
+            )
+
             break
 
+        # ---------------------------------
+
         else:
-            print("Opción no válida.")
+
+            print(
+                "Opción no válida."
+            )
 
 
 if __name__ == "__main__":
+
     main()
