f8c996b3368a2ea99e59d5f5372b631309fba2cf
alexarguellosoto <alexarguellosoto@gmail.com>
2026-08-15 03:22:59 -0600

    Refactorizar estructura y agregar procesamiento de texto

---
+++ src/preprocessing/text_cleaner.py
@@ -0,0 +1,25 @@
+class TextCleaner:
+    def clean(self, text):
+        """
+        Proceso de limpieza.
+        """
+        text = self.normalize(text)
+        text = self.remove_symbols(text)
+        return text
+
+    def normalize(self, text):
+        """
+        Convierte el texto a minúsculas.
+        """
+        return text.lower()
+
+    def remove_symbols(self, text):
+        """
+        Elimina signos de puntuación.
+        """
+        symbols = ",.;:¡!¿?()[]{}\"'"
+
+        for symbol in symbols:
+            text = text.replace(symbol, "")
+
+        return text
